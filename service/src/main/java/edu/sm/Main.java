c605836e55facd1825450eb80cc1da3a6af7f24c
joomin2 <joomin109@naver.com>
2024-10-02 16:57:13 +0900

    사용자모드 환경설정

---
+++ service/src/main/java/edu/sm/Main.java
@@ -0,0 +1,10 @@
+package edu.sm;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("1팀 사용자 모드 메인 모듈 클래스");
+    }
+}
